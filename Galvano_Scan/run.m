928c0690e77906f862d69808be069a81fda1acdf
revevevers <159438162+revevevers@users.noreply.github.com>
2025-04-09 23:41:20 +0800

    first merge

---
+++ Galvano_Scan/run.m
@@ -0,0 +1,20 @@
+
+closeup();
+
+main();
+
+%% 
+
+closeup();
+
+snake_pattern('COM5', 115200, 0.1, 0.1, 0.5, 0.5);
+% snake_pattern(serialPort, baudRate, xStepSize, yStepSize, maxAngle, pauseTime)
+
+%% 
+
+closeup();
+
+move_to_angle('COM5', 115200, 1 , -1);
+% move_to_angle(serialPort, baudRate, angleX, angleY)
+%%
+closeup();
