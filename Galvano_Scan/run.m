d5f8934a1f5bae3d1912ad832848eca1316b726c
revevevers <159438162+revevevers@users.noreply.github.com>
2025-05-21 19:37:00 +0800

    QAQ

--- Galvano_Scan/run.m
+++ Galvano_Scan/run.m
@@ -1,11 +1,8 @@
 
-
-
-
 closeup();
 %% 
 clear s;
 move_to_position('COM3', 115200, 50, 0, 163);
-
+% move_to_position(serialPort, baudRate, focusX, focusY, focalLength)
 %% 
 main(dataPacketType);
